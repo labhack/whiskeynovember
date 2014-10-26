0b0ca1cdc84adbd5c49178f2dc36166b19f0c516
Sam Raudabaugh <sraudabaugh@gmail.com>
2014-10-26 15:07:21 +0000

    Improve db speed

--- model.py
+++ model.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 
 engine = create_engine('postgresql://wn:wn@localhost:5432/wndb')
 
-from sqlalchemy import Column, Integer, Float, DateTime, Boolean, String, MetaData
+from sqlalchemy import Column, Integer, Float, DateTime, Boolean, String, MetaData, Table
 
 metadata = MetaData()
-table = Table('obs', metadata, Column(Integer, primary_key=True),
+table = Table('obs', metadata, Column('id', Integer, primary_key=True),
     Column('station_name',String),
     Column('x',Float),
     Column('y',Float),
--- process_zip.sh
+++ process_zip.sh
@@ -4,7 +4,9 @@ mkdir -p tmp/$1
 unzip $1 -d tmp/$1
 cd tmp/$1
 XYZ="$(cat *o | grep 'POSITION XYZ' | awk '{print $1,$2,$3}' | sed -e 's/ /, /g')"
+echo "Running RinDump..."
 RinDump *o ELE S1C --timefmt "%02d-%02m-%04Y %02H:%02M:%02S" --ref "\"${XYZ}"\" --eph *.sp3 > ../../$1.rindump
+echo "RinDump complete."
 cd ../..
 python detect_jam.py $1.rindump
 rm -rf tmp/*
