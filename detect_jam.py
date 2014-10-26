9483e20e773a168a516303761bd8d2cec88571ea
Sam Raudabaugh <sraudabaugh@gmail.com>
2014-10-26 06:26:01 +0000

    Integrating detection algorithm.

--- detect_jam.py
+++ detect_jam.py
@@ -3,6 +3,20 @@
 import pandas as pd
 import numpy as np
 import sys
+import model
+from sqlalchemy.orm import sessionmaker
+from model import engine
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def get_or_create(session, mdl, **kwargs):
+    instance = session.query(mdl).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = mdl(**kwargs)
+        session.add(instance)
+        return instance
 
 # process command line args
 if len(sys.argv) != 2:
@@ -28,8 +42,8 @@ else:
 # get the station name
 line = fid.readline()
 temp = line.split()
-station_name = temp[-1][0:4]
-station_name = station_name.upper()
+station = temp[-1][0:4]
+station = station.upper()
 
 # read the data into a pandas data structure
 try: 
@@ -51,8 +65,11 @@ data.loc[data_idx,'S1C'] += (17.0/50.0)*(50.0-data.loc[data_idx,'ELE'])
 sats = np.unique(data['sat'])
 
 # compute the jamming metrics
-jam_indicator = (data['S1C'].median() - pd.rolling_mean(data['S1C'], 30).values) > 4
-jam_intensity = data['S1C'].median() - pd.rolling_mean(data['S1C'], 30).values
-date_time = data['date_time'].values
+indicator = (data['S1C'].median() - pd.rolling_mean(data['S1C'], 30).values) > 4
+intensity = data['S1C'].median() - pd.rolling_mean(data['S1C'], 30).values
+date = data['date_time'].values
 
+for idx, record in enumerate(date):
+    obs = get_or_create(session, model.Observation, x=X, y=Y, z=Z, station_name=station, jam_indicator=True if indicator[idx].astype(int) == 1 else False, jam_intensity=intensity[idx], date_time=pd.to_datetime(date[idx]))
 
+session.commit()
--- process_zip.sh
+++ process_zip.sh
@@ -6,6 +6,7 @@ cd tmp/$1
 XYZ="$(cat *o | grep 'POSITION XYZ' | awk '{print $1,$2,$3}' | sed -e 's/ /, /g')"
 RinDump *o ELE S1C --timefmt "%02d-%02m-%04Y %02H:%02M:%02S" --ref "\"${XYZ}"\" --eph *.sp3 > ../../$1.rindump
 cd ../..
+python detect_jam.py $1.rindump
 rm -rf tmp/*
 rm *.zip
 echo "$1 processed"
